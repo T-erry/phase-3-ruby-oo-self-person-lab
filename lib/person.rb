734abb34d65e19b855e289c40a4f39b1b5228ac0
T-erry <terrywachuka10@gmail.com>
2023-02-11 02:28:26 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,100 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
+    end
+  
+    def bank_account
+      @bank_account
+    end
+  
+    def bank_account=(value)
+      @bank_account = value
+    end
+  
+    def happiness
+      @happiness
+    end
+  
+    def happiness=(value)
+      if value < 0
+        @happiness = 0
+      elsif value > 10
+        @happiness = 10
+      else
+        @happiness = value
+      end
+    end
+  
+    def hygiene
+      @hygiene
+    end
+  
+    def hygiene=(value)
+      if value < 0
+        @hygiene = 0
+      elsif value > 10
+        @hygiene = 10
+      else
+        @hygiene = value
+      end
+    end
+
+    def clean?
+       if @hygiene > 7
+        return true
+
+        else
+           return false
+        end
+    end
+    def happy?
+      if  @happiness > 7 
+        return true
+
+        else
+          return  false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+  end
+  def take_bath
+    self.hygiene += 4
+ "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  def work_out 
+    self.happiness += 2
+    self.hygiene -= 3
+     "♪ another one bites the dust ♫"
+  end
+  def call_friend(friend)
+    friend.happiness += 3
+    self.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+  def start_conversation(friend, topic)
+    if topic == "politics"
+        friend.happiness -= 2
+        self.happiness -= 2
+        "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+        friend.happiness += 1
+        self.happiness += 1
+        "blah blah sun blah rain"
+    else
+        "blah blah blah blah blah" 
+    end
+  end
+
+end
+  
+person1 = Person.new("Terry")
